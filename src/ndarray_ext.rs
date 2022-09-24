75c950111d24d2b0af56887775b5a4bf44c779e2
Rohith04MVK <007rohitjj04@gmail.com>
2022-09-24 20:15:03 +0530

    Added more ndarray helper functions

--- src/ndarray_ext.rs
+++ src/ndarray_ext.rs
@@ -1,33 +1,34 @@
-extern crate ndarray;
+use ndarray;
 
-pub type NdArray = ndarray::Array<f32, ndarray::IxDyn>;
+// expose array_gen
+use crate::Float;
 
-pub type NdArrayView<'a> = ndarray::ArrayView<'a, f32, ndarray::IxDyn>;
+/// alias for `ndarray::Array<T, IxDyn>`
+pub type NdArray<T> = ndarray::Array<T, ndarray::IxDyn>;
+
+/// alias for `ndarray::ArrayView<T, IxDyn>`
+pub type NdArrayView<'a, T> = ndarray::ArrayView<'a, T, ndarray::IxDyn>;
+
+/// alias for `ndarray::ArrayViewMut<T, IxDyn>`
+pub type NdArrayViewMut<'a, T> = ndarray::ArrayViewMut<'a, T, ndarray::IxDyn>;
+
+use rand_xorshift;
 
 #[inline]
-pub fn arr_to_shape(arr: &NdArray) -> Vec<usize> {
-    arr.iter().map(|&a| a as usize).collect::<Vec<_>>()
+/// This works well only for small arrays
+pub(crate) fn as_shape<T: Float>(x: &NdArrayView<T>) -> Vec<usize> {
+    x.iter().map(|a| a.to_usize().unwrap()).collect()
 }
 
-#[doc(hidden)]
 #[inline]
-pub fn expand_dims_view<'a>(x: NdArrayView<'a>, axis: usize) -> NdArrayView<'a> {
+pub(crate) fn expand_dims<T: Float>(x: NdArray<T>, axis: usize) -> NdArray<T> {
     let mut shape = x.shape().to_vec();
     shape.insert(axis, 1);
     x.into_shape(shape).unwrap()
 }
 
-#[doc(hidden)]
 #[inline]
-pub fn expand_dims(x: NdArray, axis: usize) -> NdArray {
-    let mut shape = x.shape().to_vec();
-    shape.insert(axis, 1);
-    x.into_shape(shape).unwrap()
-}
-
-#[doc(hidden)]
-#[inline]
-pub fn roll_axis(arg: &mut NdArray, to: ndarray::Axis, from: ndarray::Axis) {
+pub(crate) fn roll_axis<T: Float>(arg: &mut NdArray<T>, to: ndarray::Axis, from: ndarray::Axis) {
     let i = to.index();
     let mut j = from.index();
     if j > i {
@@ -40,5 +41,14 @@ pub fn roll_axis(arg: &mut NdArray, to: ndarray::Axis, from: ndarray::Axis) {
             arg.swap_axes(i, j);
             j += 1;
         }
+    }
+}
+
+#[inline]
+pub(crate) fn normalize_negative_axis(axis: isize, ndim: usize) -> usize {
+    if axis < 0 {
+        (ndim as isize + axis) as usize
+    } else {
+        axis as usize
     }
 }
