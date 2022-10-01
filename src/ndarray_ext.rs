36226c5f6e99ecee479e1a9a7bb3e51ab541a7fb
Rohith04MVK <007rohitjj04@gmail.com>
2022-10-01 20:06:51 +0530

    Added more ndarray helper functions

--- src/ndarray_ext.rs
+++ src/ndarray_ext.rs
@@ -19,7 +19,7 @@ use rand_xorshift;
 pub(crate) fn as_shape<T: Float>(x: &NdArrayView<T>) -> Vec<usize> {
     x.iter().map(|a| a.to_usize().unwrap()).collect()
 }
- 
+
 #[inline]
 pub(crate) fn expand_dims<T: Float>(x: NdArray<T>, axis: usize) -> NdArray<T> {
     let mut shape = x.shape().to_vec();
@@ -51,4 +51,68 @@ pub(crate) fn normalize_negative_axis(axis: isize, ndim: usize) -> usize {
     } else {
         axis as usize
     }
+}
+
+#[inline]
+pub(crate) fn normalize_negative_axes<T: Float>(axes: &NdArrayView<T>, ndim: usize) -> Vec<usize> {
+    let mut axes_ret: Vec<usize> = Vec::with_capacity(axes.len());
+    for &axis in axes.iter() {
+        let axis = if axis < T::zero() {
+            (T::from(ndim).unwrap() + axis).to_usize().unwrap()
+        } else {
+            axis.to_usize().unwrap()
+        };
+        axes_ret.push(axis);
+    }
+    axes_ret
+}
+
+#[inline]
+pub(crate) fn sparse_to_dense<T: Float>(arr: &NdArrayView<T>) -> Vec<usize> {
+    let mut axes: Vec<usize> = vec![];
+    for (i, &a) in arr.iter().enumerate() {
+        if a == T::one() {
+            axes.push(i);
+        }
+    }
+    axes
+}
+
+#[allow(unused)]
+#[inline]
+pub(crate) fn is_fully_transposed(strides: &[ndarray::Ixs]) -> bool {
+    let mut ret = true;
+    for w in strides.windows(2) {
+        if w[0] > w[1] {
+            ret = false;
+            break;
+        }
+    }
+    ret
+}
+
+#[inline]
+pub(crate) fn copy_if_not_standard<T: Float>(x: &NdArrayView<T>) -> Option<NdArray<T>> {
+    if !x.is_standard_layout() {
+        Some(deep_copy(x))
+    } else {
+        None
+    }
+}
+
+#[inline]
+pub(crate) fn deep_copy<T: Float>(x: &NdArrayView<T>) -> NdArray<T> {
+    let vec = x.iter().cloned().collect::<Vec<_>>();
+    NdArray::from_shape_vec(x.shape(), vec).unwrap()
+}
+
+#[inline]
+pub(crate) fn scalar_shape<T: Float>() -> NdArray<T> {
+    // safe unwrap
+    NdArray::from_shape_vec(ndarray::IxDyn(&[0]), vec![]).unwrap()
+}
+
+#[inline]
+pub(crate) fn is_scalar_shape(shape: &[usize]) -> bool {
+    shape == [] || shape == [0]
 }
